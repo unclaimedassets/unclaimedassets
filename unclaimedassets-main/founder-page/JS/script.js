359b812a8c3b2a8828b9cce34e77498be533f74e
unclaimedassets <154282308+unclaimedassets@users.noreply.github.com>
2024-05-19 21:21:01 -0400

    Add files via upload

---
+++ unclaimedassets-main/founder-page/JS/script.js
@@ -0,0 +1,25 @@
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+$(function(){
+    //WOW plugin init
+    new WOW().init();
+
+    //parallax effect for banner
+    (function() {
+        var posY;
+        var image = document.getElementById('parallax');;
+        function paralax() {
+            posY = window.pageYOffset;
+            image.style.top = posY * 0.9 + 'px';
+        }
+        window.addEventListener('scroll', paralax);
+    })();
+});
